32c7781b19dbff2c0e86f92ac6e17005915690d2
Adesh Sarker <happycodding.main@gmail.com>
2022-12-31 11:33:34 +0600

    project upload

---
+++ assets/vendor/js/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $("#featuresClick").click(function () {
+        $("#features").toggle();
+        $("#info").hide();
+        $("#popular").hide();
+    });
+    $("#infoClick").click(function () {
+        $("#info").toggle();
+        $("#features").hide();
+        $("#popular").hide();
+    });
+    $("#popularClick").click(function () {
+        $("#popular").toggle();
+        $("#info").hide();
+        $("#features").hide();
+    });
+    $("#priceClick").click(function () {
+        $("#price").toggle();
+       
+    });
+    $("#categoriesClick").click(function () {
+        $("#categories").toggle();
+       
+    });
+    $("#ratingsClick").click(function () {
+        $("#ratings").toggle();
+       
+    });
+    $("#sizeClick").click(function () {
+        $("#size").toggle();
+       
+    });
+});
